57096fbacf13b24e6ad01d1bce0e6f26447db8d5
Marsh Ray <marshray@live.com>
2023-10-30 23:50:11 -0700

    Initial Sunangle

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "sunangle",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(sunangle::SunangleApp::new(cc))),
     )
 }
 
@@ -31,7 +31,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(sunangle::SunangleApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
